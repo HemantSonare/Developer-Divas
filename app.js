0b6f8828a37272239f80c7732b7acd52bf2afd9b
HemantSonare <hemantkumarsonare@gmail.com>
2025-11-20 18:26:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // Firebase config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAJwFJqUFkrl9gSrwpNALXFv9VuQKuunys",
+  authDomain: "developer-divas.firebaseapp.com",
+  projectId: "developer-divas",
+  storageBucket: "developer-divas.firebasestorage.app",
+  messagingSenderId: "468902482726",
+  appId: "1:468902482726:web:bedbd34b528557bbf727e7",
+  measurementId: "G-W96RWLW9MF"
 };
 firebase.initializeApp(firebaseConfig);
 
@@ -13,7 +14,12 @@ const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
 
-const allowedUsers = ["user1@gmail.com", "user2@gmail.com", "user3@gmail.com"];
+// ✅ Allowed Gmail users
+const allowedUsers = [
+  "hemantkumarsonare@gmail.com",
+  "shivangimhaski@gmail.com",
+  "ajitsonare@gmail.com"
+];
 
 // Elements
 const loginBtn = document.getElementById("login-btn");
@@ -43,6 +49,7 @@ auth.onAuthStateChanged(user => {
   } else {
     loginScreen.classList.remove("hidden");
     chatScreen.classList.add("hidden");
+    if(user) alert("Access restricted!");
   }
 });
 
